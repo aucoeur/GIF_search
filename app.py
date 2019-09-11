e08be36e69e02f35c68633a72f4d7c9b4a873c39
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-09-10 20:32:21 -0700

    Add Tenor API key

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import json
 
 app = Flask(__name__)
 
+apikey = "7YRWBT7DN78Q"
+
 @app.route('/')
 def index():
     """Return homepage."""
+    section = 'WELCOME TO GIF SEARCH'
+    subtitle = 'compliments of Tenor API'
+
     # TODO: Extract query term from url
 
     # TODO: Make 'params' dict with query term and API key
@@ -14,10 +19,16 @@ def index():
     # TODO: Make an API call to Tenor using the 'requests' library
 
     # TODO: Get the first 10 results from the search results
-
+    
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
-    return render_template("index.html")
+    return render_template('index.html', 
+        section = section,
+        subtitle = subtitle)
+
+# @app.route('/gif')
+# def get_gif():
+
 
 if __name__ == '__main__':
     app.run(debug=True)
