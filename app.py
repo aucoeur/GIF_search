92b1d9780376bcce605e72720cbe5a08fa1251a7
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-09-11 21:06:05 -0700

    Display single gif on home page

--- app.py
+++ app.py
@@ -4,27 +4,36 @@ import json
 
 app = Flask(__name__)
 
-apikey = "7YRWBT7DN78Q"
+
 
 @app.route('/')
 def index():
     """Return homepage."""
+
     section = 'WELCOME TO GIF SEARCH'
     subtitle = 'compliments of Tenor API'
+    stuff = 'Dear Diary,<br /><br />It\'s me, Laganja.  <br /><br />Today all the girls sat separate from me and I lived alone under a table.<br /><br />'
+    
 
     # TODO: Extract query term from url
-
+    query = 'laganja'
     # TODO: Make 'params' dict with query term and API key
-
+    params = {'q': query,
+        'key': '7YRWBT7DN78Q',
+        'limit': '1'}
     # TODO: Make an API call to Tenor using the 'requests' library
-
+    r = requests.get("https://api.tenor.com/v1/search", params=params)
     # TODO: Get the first 10 results from the search results
-    
+    gif_json = r.json()
+    gif_output = gif_json['results']
+
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
     return render_template('index.html', 
         section = section,
-        subtitle = subtitle)
+        subtitle = subtitle,
+        stuff = stuff,
+        gif_output = gif_output)
 
 # @app.route('/gif')
 # def get_gif():
